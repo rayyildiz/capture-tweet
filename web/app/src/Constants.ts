a264387da600185693e169f756cf90887055f851
Ramazan AYYILDIZ <rayyildiz@gmail.com>
2020-06-27 17:12:00 +0200

    ready for deployment

--- web/app/src/Constants.ts
+++ web/app/src/Constants.ts
@@ -1,1 +1,15 @@
-export const BASE_API = process.env.BASE_API || '/api/query';
+const prod = {
+  enableSW: true,
+  apiURL: 'https://beta-api.capturetweet.com/api/query'
+};
+
+const dev = {
+  enableSW: false,
+  apiURL: '/api/query'
+};
+
+const config = process.env.NODE_ENV === 'development' ? dev : prod;
+
+
+export const BASE_API = config.apiURL;
+export const ENABLE_SW = config.enableSW;
