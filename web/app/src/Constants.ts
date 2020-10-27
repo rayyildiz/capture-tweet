79423fa5aea3968ba9bfea4146f7b1679d4fab61
Ramazan AYYILDIZ <rayyildiz@gmail.com>
2020-10-27 23:35:28 +0100

    react 17

--- web/app/src/Constants.ts
+++ web/app/src/Constants.ts
@@ -1,11 +1,13 @@
 const prod = {
   enableSW: false,
+  trackMonitoring: false,
   apiURL: 'https://api.capturetweet.com/api/query',
   captchaKey: '6LeH-rYZAAAAAE23jskkX4U2_oJoXvcreHg2n2ic'
 };
 
 const dev = {
   enableSW: false,
+  trackMonitoring: true,
   apiURL: '/api/query',
   captchaKey: '6LeH-rYZAAAAAE23jskkX4U2_oJoXvcreHg2n2ic'
 };
@@ -14,5 +16,6 @@ const config = process.env.NODE_ENV === 'development' ? dev : prod;
 
 export const BASE_API = config.apiURL;
 export const ENABLE_SW = config.enableSW;
+export const ENABLE_MONITORING = config.trackMonitoring;
 export const CAPTCHA_KEY = config.captchaKey;
 export const WEB_BASE_URL = 'https://capturetweet.com';
--- web/app/src/pages/TweetPage.tsx
+++ web/app/src/pages/TweetPage.tsx
@@ -52,7 +52,7 @@ const TWEET_BY_ID = gql`
 
 
 const TweetPage: FC = () => {
-  const {id} = useParams();
+  const {id} = useParams<{ id: string }>();
 
   const {data, loading, error} = useQuery<Tweet, TweetVariables>(TWEET_BY_ID, {
     variables: {id}
@@ -126,7 +126,7 @@ const TweetDetail: FC<TweetDetailProps> = ({tweet}) => {
               <button type="button" className="btn btn-link" onClick={openModal}>
               <span data-toggle="tooltip" data-placement="top" title="Report this tweet">
                  <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-info-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path fillRule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                 <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588z"/>
                 <circle cx="8" cy="4.5" r="1"/>
               </svg>
@@ -156,9 +156,7 @@ const TweetDetail: FC<TweetDetailProps> = ({tweet}) => {
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title" id="reportTweetModalLabel"> Report Tweet </h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
+                <button type="button" className="btn-close" data-dismiss="modal" aria-label="Close"></button>
               </div>
               <div className="modal-body">
                 <ContactPage tweetID={tweet.id}/>
@@ -175,27 +173,26 @@ type TweetImageCardProps = {
   id: string
 }
 const TweetImageCard: FC<TweetImageCardProps> = ({id}) => {
-  const {data, startPolling, stopPolling} = useQuery<TweetImage, TweetImageVariables>(TWEET_IMAGE, {
+  const {data, stopPolling} = useQuery<TweetImage, TweetImageVariables>(TWEET_IMAGE, {
     variables: {
       id
-    },
+    }, pollInterval: 2000
   })
 
-  useEffect(() => {
-    startPolling(1900);
-    setTimeout(() => {
-      stopPolling();
-    }, 10 * 1900);
+  setTimeout(() => {
+    stopPolling();
+  }, 10 * 1900);
 
-    return () => stopPolling();
-  }, [id, startPolling, stopPolling])
+  useEffect(() => {
+    return () => {
+      stopPolling();
+    }
+  }, [id, stopPolling])
 
   if (data && data?.tweet && data.tweet.captureURL) {
-    stopPolling();
-
     return (<img src={`/${data.tweet.captureURL}`} alt="" className="img-fluid"/>);
-
   }
+
 
   return (
       <>
