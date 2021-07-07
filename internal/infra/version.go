ac99d9949c1fd658ef2b56e89577cadef9f8236d
Ramazan AYYILDIZ <rayyildiz@gmail.com>
2021-07-07 15:44:34 +0300

    typo

--- internal/infra/version.go
+++ internal/infra/version.go
@@ -1,3 +1,14 @@
 package infra
 
+import "net/http"
+
 var Version string // do not remove or modify
+
+func VersionHandler(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("x-api-version", Version)
+		h.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+
+}
