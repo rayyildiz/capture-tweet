5c89e5f26214a82002822ff7cb8fd99772779349
Ramazan AYYILDIZ <rayyildiz@gmail.com>
2021-08-17 11:44:34 +0300

    show username/screenName

--- web/app/src/pages/SearchPage.tsx
+++ web/app/src/pages/SearchPage.tsx
@@ -65,7 +65,7 @@ const SearchPage = () => {
 
       <div className="row">
         {data && data.search?.map(t => (t && <TweetCard key={t.id}
-                                                        author={t.author?.screenName || t.author?.userName}
+                                                        author={t.author?.screenName ?? t.author?.userName}
                                                         fullText={t.fullText ?? ""}
                                                         id={t.id ?? ""}
                                                         captureThumbURL={t.captureThumbURL ?? ""}
