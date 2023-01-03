1b47be756f8790442ba66308b041438d51a4f15e
Ramazan A <r@ayyildiz.ai>
2023-01-03 10:44:27 +0300

    new handler , eventarc

--- cmd/thumb/handler.go
+++ cmd/thumb/handler.go
@@ -18,19 +18,24 @@ import (
 	"gocloud.dev/blob"
 )
 
-type PubSubMessage struct {
-	Message struct {
-		Data []byte `json:"data,omitempty"`
-		ID   string `json:"id"`
-	} `json:"message"`
-	Subscription string `json:"subscription"`
-}
-
-type StorageMessage struct {
-	Name        string `json:"name"`
-	Kind        string `json:"kind"`
-	ContentType string `json:"contentType"`
-	Size        string `json:"size"`
+type EventArcRequest struct {
+	Kind                    string    `json:"kind"`
+	Id                      string    `json:"id"`
+	SelfLink                string    `json:"selfLink"`
+	Name                    string    `json:"name"`
+	Bucket                  string    `json:"bucket"`
+	Generation              string    `json:"generation"`
+	Metageneration          string    `json:"metageneration"`
+	ContentType             string    `json:"contentType"`
+	TimeCreated             time.Time `json:"timeCreated"`
+	Updated                 time.Time `json:"updated"`
+	StorageClass            string    `json:"storageClass"`
+	TimeStorageClassUpdated time.Time `json:"timeStorageClassUpdated"`
+	Size                    string    `json:"size"`
+	Md5Hash                 string    `json:"md5Hash"`
+	MediaLink               string    `json:"mediaLink"`
+	Crc32C                  string    `json:"crc32c"`
+	Etag                    string    `json:"etag"`
 }
 
 type handlerImpl struct {
@@ -38,7 +43,7 @@ type handlerImpl struct {
 	bucket  *blob.Bucket
 }
 
-func (h handlerImpl) handleResizeLog(w http.ResponseWriter, r *http.Request) {
+func (h handlerImpl) handleLog(w http.ResponseWriter, r *http.Request) {
 	s := fmt.Sprintf("Detected change in Cloud Storage bucket: %s", r.Header.Get("Ce-Subject"))
 	zap.L().Info(s)
 	defer r.Body.Close()
@@ -63,7 +68,7 @@ func (h handlerImpl) handleResize(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var payload PubSubMessage
+	var payload EventArcRequest
 	err := json.NewDecoder(r.Body).Decode(&payload)
 	if err != nil {
 		zap.L().Error("bad request", zap.Error(err))
@@ -72,16 +77,15 @@ func (h handlerImpl) handleResize(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
-	request := StorageMessage{}
-	err = json.Unmarshal(payload.Message.Data, &request)
-	if err != nil {
-		zap.L().Error("bad request, decode payload.data", zap.Error(err))
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+	if !strings.HasPrefix(payload.Name, "capture/large/") {
+		zap.L().Warn("not a valid prefix", zap.String("image_kind", payload.Kind), zap.String("name", payload.Name), zap.String("id", payload.Id))
+		w.WriteHeader(http.StatusNoContent)
+		fmt.Fprintf(w, "")
 		return
 	}
 
-	if request.Kind != "storage#object" {
-		zap.L().Warn("expected image kind must be object", zap.String("image_kind", request.Kind), zap.String("image_key", request.Name))
+	if payload.Kind != "storage#object" {
+		zap.L().Warn("expected image kind must be object", zap.String("image_kind", payload.Kind), zap.String("name", payload.Name), zap.String("id", payload.Id))
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte("it is not an object"))
 		return
@@ -90,16 +94,16 @@ func (h handlerImpl) handleResize(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(r.Context(), time.Second*10)
 	defer cancel()
 
-	img, err := h.bucket.ReadAll(ctx, request.Name)
+	img, err := h.bucket.ReadAll(ctx, payload.Name)
 	if err != nil {
-		zap.L().Error("open bucket", zap.String("image_key", request.Name), zap.String("image_kind", request.Kind), zap.Error(err))
+		zap.L().Error("open bucket", zap.String("image_key", payload.Name), zap.String("image_kind", payload.Kind), zap.Error(err))
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
-	attrs, err := h.bucket.Attributes(ctx, request.Name)
+	attrs, err := h.bucket.Attributes(ctx, payload.Name)
 	if err != nil {
-		zap.L().Error("read image attributes", zap.String("image_key", request.Name), zap.String("image_kind", request.Kind), zap.Error(err))
+		zap.L().Error("read image attributes", zap.String("image_key", payload.Name), zap.String("image_kind", payload.Kind), zap.Error(err))
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
@@ -110,12 +114,12 @@ func (h handlerImpl) handleResize(w http.ResponseWriter, r *http.Request) {
 
 	decoder, _, err := image.Decode(bytes.NewBuffer(img))
 	if err != nil {
-		zap.L().Error("image decode", zap.String("image_key", request.Name), zap.String("image_kind", request.Kind), zap.Error(err))
+		zap.L().Error("image decode", zap.String("image_key", payload.Name), zap.String("image_kind", payload.Kind), zap.Error(err))
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
-	split := strings.Split(request.Name, "/")
+	split := strings.Split(payload.Name, "/")
 	fileName := split[len(split)-1]
 	thumbNailKey := fmt.Sprintf("capture/thumb/%s", fileName)
 
@@ -124,7 +128,7 @@ func (h handlerImpl) handleResize(w http.ResponseWriter, r *http.Request) {
 	buf := new(bytes.Buffer)
 	err = jpeg.Encode(buf, newImage, nil)
 	if err != nil {
-		zap.L().Error("jpeg encode image", zap.String("image_key", request.Name), zap.String("image_kind", request.Kind), zap.Error(err))
+		zap.L().Error("jpeg encode image", zap.String("image_key", payload.Name), zap.String("image_kind", payload.Kind), zap.Error(err))
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
@@ -140,20 +144,20 @@ func (h handlerImpl) handleResize(w http.ResponseWriter, r *http.Request) {
 		},
 	})
 	if err != nil {
-		zap.L().Error("open bucket for thumbnail", zap.String("image_key", request.Name), zap.String("image_kind", request.Kind), zap.Error(err))
+		zap.L().Error("open bucket for thumbnail", zap.String("image_key", payload.Name), zap.String("image_kind", payload.Kind), zap.Error(err))
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
-	zap.L().Info("image stored successfully", zap.String("image_key", thumbNailKey), zap.String("image_key", request.Name), zap.String("tweet_id", tweetId), zap.String("tweet_user", tweetUser))
+	zap.L().Info("image stored successfully", zap.String("image_key", thumbNailKey), zap.String("image_key", payload.Name), zap.String("tweet_id", tweetId), zap.String("tweet_user", tweetUser))
 	err = h.service.UpdateThumbImage(ctx, tweetId, thumbNailKey)
 	if err != nil {
-		zap.L().Error("save in database", zap.String("image_key", request.Name), zap.String("image_kind", request.Kind), zap.Error(err))
+		zap.L().Error("save in database", zap.String("image_key", payload.Name), zap.String("image_kind", payload.Kind), zap.Error(err))
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 		return
 	}
 
-	zap.L().Info("image saved", zap.Duration("elapsed", time.Since(start).Round(time.Millisecond)), zap.String("image_thumb", thumbNailKey), zap.String("image_key", request.Name), zap.String("image_kind", request.Kind))
+	zap.L().Info("image saved", zap.Duration("elapsed", time.Since(start).Round(time.Millisecond)), zap.String("image_thumb", thumbNailKey), zap.String("image_key", payload.Name), zap.String("image_kind", payload.Kind))
 
 	w.WriteHeader(http.StatusCreated)
 	_, _ = w.Write([]byte("No Content"))
